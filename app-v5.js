75e9b1d82da561587e51fb924a169d24cf32b7ad
tuesso <tuesso1982@gmail.com>
2026-09-04 22:13:45 +0500

    Подключить V6 Adventure Mode к основному приложению

--- app-v5.js
+++ app-v5.js
@@ -1,2 +1,8 @@
-/* V5 bootstrap: core + Alan/Hector personalization */
-(()=>{const load=(src)=>new Promise((ok,fail)=>{const s=document.createElement('script');s.src=src;s.onload=ok;s.onerror=fail;document.head.appendChild(s)});load('./app-core.js').then(()=>load('./alan-hector.js')).catch(console.error)})();
+/* V6 bootstrap: core + Alan/Hector + Adventure Mode */
+(()=>{
+  const load=(src)=>new Promise((ok,fail)=>{const s=document.createElement('script');s.src=src;s.onload=ok;s.onerror=fail;document.head.appendChild(s)});
+  load('./app-core.js')
+    .then(()=>load('./alan-hector.js'))
+    .then(()=>load('./v6-game.js'))
+    .catch(console.error);
+})();
