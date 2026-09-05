5af7e15c7aa7813fc0ffed29aebef6f027d0d503
tuesso <tuesso1982@gmail.com>
2026-09-05 18:18:28 +0500

    Refresh offline cache for expanded sentence game

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="eublephar-v6.0-adventure";
+const CACHE="eublephar-v6.1-sentences-90";
 const ASSETS=["./","./index.html","./styles-v5.css","./styles-core.css","./alan-hector.css","./v6-game.css","./app-v5.js","./app-core.js","./alan-hector.js","./v6-game.js","./manifest.webmanifest","./gecko-real.webp","./icon-192.png","./icon-512.png"];
 self.addEventListener("install",event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener("activate",event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
